5bd7eeea0c2dac0d79ec70c0a4517fa2d3b43671
Windless95 <barrerajoelj@gmail.com>
2021-12-14 01:59:57 +0000

    fixed array

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  document.querySelector(".board").innerHTML = new Array(9)
-    .fill(null)
-    .map(elm => drawGrid("times"))
+  document.querySelector(".board").innerHTML = board
+    .map(elm => drawGrid(elm))
     .join("");
 };
 
+let board = [null, null, null, null, null, null, null, null, null];
 function drawGrid(symbol = "times") {
   return `<div class= "grid">
   <i class="fas fa-${symbol}"></i>
