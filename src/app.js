9d485fedcf9340c41850d819b4641cb98c4f73dd
Windless95 <barrerajoelj@gmail.com>
2021-12-16 00:24:46 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,20 +1,43 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// model or store = data that needs to be share among my front end (no need for local variables)
+// let board = [null, "times", null, null, null, "circle", null, null, null];
+let board = [null, null, null, null, null, null, null, null, null];
+let currentPlayer = "times";
 
-window.onload = function() {
+// life-cycle
+window.onload = render;
+
+function render() {
   document.querySelector(".board").innerHTML = board
     .map(elm => drawGrid(elm))
     .join("");
-};
 
-let board = [null, null, null, null, null, null, null, null, null];
+  document.querySelectorAll(".grid").forEach((node, index) =>
+    node.addEventListener("click", function() {
+      play(index);
+    })
+  );
+}
+
+// action fucntion
+function play(index) {
+  board[index] = currentPlayer;
+
+  if (currentPlayer === "times") {
+    currentPlayer = "circle";
+  } else {
+    currentPlayer = "times";
+  }
+  // currentPlayer = (currentPlayer === "times") ? "circle" : "times";
+
+  render();
+}
+
+// rendering function
 function drawGrid(symbol = "times") {
-  return `<div class= "grid">
+  return `<div class="grid">
   <i class="fas fa-${symbol}"></i>
-  </div>
-  `;
+</div>`;
 }
